9758c3392e7c725a8ff2c6e318d225c420b1eb9b
Freika <iamfrey@gmail.com>
2015-07-26 15:26:23 +0600

    Add new photo page.

---
+++ app/controllers/photos/new.js
@@ -0,0 +1,32 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+  isValid: Ember.computed(
+    'model.title',
+    'model.description',
+    {
+      get() {
+        return !Ember.isEmpty(this.get('model.title')) &&
+               !Ember.isEmpty(this.get('model.title'));
+      }
+    }
+  ),
+  actions: {
+    save() {
+      if (this.get('isValid')) {
+        this.get('model').save().then((photo) => {
+          this.transitionToRoute('photos.show', photo);
+        });
+      } else {
+        this.set('errorMessage', 'You have to fill all the fields');
+      }
+
+      return false;
+    },
+    cancel() {
+      this.transitionToRoute('photos');
+
+      return false;
+    }
+  }
+})
